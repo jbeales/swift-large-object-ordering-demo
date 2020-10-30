eadb3307621857e69087cf41d85088d6db8f0dbc
John Beales <john@johnbeales.com>
2020-10-30 09:10:53 -0400

    Update to use a text file so we can look at the contents and see if they're out of order.

--- main.php
+++ main.php
@@ -10,6 +10,20 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 
+function createTestObjectSource( $number_of_segments ) {
+    // The "large" object will be a text file that's $number_of_segments lines long.
+    // Each line will be 1024 bytes long. That's what we'll use as our segment size.
+    
+    $source = '';
+    for( $i=0; $i < $number_of_segments; $i++ ) {
+        $source .= str_pad($i + 1 . '  ' , 1021, '*' ) . "\n";
+    }
+
+    file_put_contents( 'test-files/object-source.txt', $source );
+}
+
+createTestObjectSource( 12 );
+
 
 // Set up openstack client
 $openstack = new OpenStack\OpenStack([
@@ -29,36 +43,60 @@ $openstack = new OpenStack\OpenStack([
 
 $options = getopt("f:");
 
-if(isset($options['f']) && $options['f'] === 'prod' ) {
-	$filename = '2020-10-29-02-45-01.zip';
-} else {
-	$filename = 'dev-2020-10-29-09-33-44.zip';
-}
+$filename = 'object-source.txt';
 
 echo "Uploading $filename\n";
 flush();
 
+$destination_name = 'debug-files/' . $filename;
 
 $options = [
-    'name'   => 'debug-files/' . $filename,
+    'name'   => $destination_name,
     'stream' => Psr7\stream_for(fopen(__DIR__ . '/test-files/' . $filename, 'r')),
+    'segmentSize' => 1024,
+    'segmentContainer' => $_ENV['OVH_SEGMENT_CONTAINER']
 ];
 
-// optional: specify the size of each segment in bytes
-$options['segmentSize'] = 1073741824;
 
-// optional: specify the container where the segments live. This does not necessarily have to be the
-// same as the container which holds the manifest file
-$options['segmentContainer'] = 'LaravelBackup-segments';
-
-
-$container = $openstack->objectStoreV1()
-                    ->getContainer('test-container');
+$container = $openstack->objectStoreV1()->getContainer($_ENV['OVH_CONTAINER']);
 
 /** @var \OpenStack\ObjectStore\v1\Models\StorageObject $object */
 $object = $container->createLargeObject($options);
 
-echo "Done.\n";
+echo "Uploaded.\n";
+echo "Downloading for verification.\n";
+
+$sourceStream = $container->getObject($destination_name)->download();
+
+$resultFileName = __DIR__ . '/test-files/object-result.txt';
+$destinationStream = Psr7\Utils::streamFor(fopen( $resultFileName, 'w'));
+Psr7\Utils::copyToStream($sourceStream, $destinationStream);
+
+// Close resources. Downloading done.
+$sourceStream->close();
+$destinationStream->close();
+echo "Downloaded.\n";
+
+echo "Compare two files.\n";
+
+$source = file_get_contents( 'test-files/object-source.txt' );
+$source_length = strlen($source);
+echo "Source, (length: $source_length): \n\n";
+echo $source;
+
+$result = file_get_contents( $resultFileName );
+$result_length = strlen($result);
+echo "Result, (length: $result_length): \n\n";
+
+echo $result;
+
+echo "\n\n\n";
+if($source === $result ) {
+    echo "The data is the same!\n";
+} else {
+    echo "The data has changed!\n";
+}
+
 
 
 
